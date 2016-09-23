2b013f61320f8005ac98659c8c3dade62e29d593
Lin, Cheng-Tao <mutolisp@gmail.com>
2016-09-23 13:42:46 +0800

    增加第一節課的 demo 檔案
    1. 撰寫 Rmd 說明文件
    2. 增加 README.md 範例

---
+++ examples/ch1_r_project/ch1.R
@@ -0,0 +1,12 @@
+# this is comments
+# this is also a comment
+# test
+# test2
+
+# 設定工作目錄
+# 如果你使用 Mac 或 GNU/Linux作業系統
+setwd('/Users/psilotum/Documents/Dropbox/Courses/生態資訊學/demo')
+# 假設你使用 windows 作業系統，路徑要改成 \\ (兩個反斜線)
+# 
+setwd('C:\\Users\\psilotum\\Documents\\Dropbox\\Courses\\生態資訊學\\demo')
+# 或是使用 Session
--- examples/ch1_r_project/ch_1_example1.R
+++
@@ -1,3 +0,0 @@
-## Chapter 1. R introduction
-#
-# 以井字號(hash=#)開頭的後面都會當成註解 
